01513df70eebd43c9714083386392afdf0f1da62
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-09-03 05:09:45 +0600

    'cat-dynamic-url'

--- js/main.js
+++ js/main.js
@@ -12,12 +12,18 @@ const loadNewsCategory = categorys => {
         // categoryDiv.classList.add('')
         categoryDiv.innerHTML = `
         <div class='category'>
-            <div class='btn p-3 fw-semibold' href='#'>${category.category_name}</div>
+            <div onclick="categoryClick(${category.category_id})" class='btn p-3 fw-semibold' href='#'>${category.category_name}</div>
         </div>`
         allCategoryFind.appendChild(categoryDiv)
+        // categoryClick(category.category_id)
     });
 }
 
+const categoryClick = async id => {
+    const url = await fetch(`https://openapi.programming-hero.com/api/news/category/0${id}`)
+    const response = await url.json();
+    console.log(response.data)
+}
 
 
 loadCategory()
