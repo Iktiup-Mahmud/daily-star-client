0f098077c97019a2a2d96890926fde5984161356
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-09-03 04:22:34 +0600

    'categorys-added'

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+const loadCategory = async () => {
+    const url = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const response = await url.json();
+    loadNewsCategory(response.data.news_category);
+}
 
+const loadNewsCategory = categorys => {
+    console.log(categorys);
+    const allCategoryFind = document.getElementById('all-category');
+    categorys.forEach( category => {
+        const categoryDiv = document.createElement('div');
+        // categoryDiv.classList.add('')
+        categoryDiv.innerHTML = `
+        <div class='category'>
+            <div class='btn p-3 fw-semibold' href='#'>${category.category_name}</div>
+        </div>`
+        allCategoryFind.appendChild(categoryDiv)
+    });
+}
+
+
+
+loadCategory()
+// loadNewsCategory()
